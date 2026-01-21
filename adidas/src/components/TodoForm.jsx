9fd4789abbd7fd4ecaae89d700cea785960117e7
Andrey <andrey.28.06@mail.ru>
2026-01-21 04:10:25 +0000

    ada

---
+++ adidas/src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function TodoForm({ onAdd }) {
+   const [inputValue, setInputValue] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    const acc = []
+
+    if (inputValue = 0){
+return acc
+    }
+    
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Введите новую задачу..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type="submit" className="btn btn-add">
+        Добавить
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
